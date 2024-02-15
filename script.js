f4026ecbd6d6777127e272e11f32e6bb7267ebf1
Darshana Birari <102860858+DaRsHana200@users.noreply.github.com>
2024-02-15 14:27:02 +0530

    changes done
    changes done

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-
-
-
-
 async function fetchWeatherCity(cityName){
 	let url=`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=fff1b2b915f69afc4b3d32ae4dc7a9df`;
-	const response=await fetch(url)
+	const response = await fetch(url)
 	const data=await response.json();
-	console.log(data);
+	let city=document.getElementById("city");
+	city.innerHTML=data.name;
+	let windSpeed=document.getElementById("wind_speed");
+	windSpeed.innerHTML=data.wind.speed+"km/hr";
+	let humidity=document.getElementById("humidity");
+	humidity.innerHTML=data.main.humidity+"g/kg";
+	let temp=document.getElementById("temp");
+	temp.innerHTML=Math.round((data.main.temp)-273.15)+"°C";
+    console.log(data)
 }
-//demo()
+
 
 function fetchCity(){
 	let cityName=document.getElementById("city_name");
-	if(cityName.value==""){
+	if(cityName.value===""){
 		alert("Enter a city name");
 	}
 	else{
